89facc23250ae3cb82c29024248155970cd0d0de
Maxim Mai <maxim.sl.mai@gmail.com>
2016-06-13 01:35:56 -0400

    added logic to handle the rooms

--- parser.py
+++ parser.py
@@ -19,33 +19,66 @@ for listing in listings:
 
         items = BeautifulSoup(listing_page.text, "html.parser").findAll('span', {'class' : 'formfield'})
 
+        # the result set of a MLS listing
         listing_json = {}
-        counter = 0 
-        for item in items:
-                #print "#", counter, ":", item
-                # start parsing the MLS line by line
-                if item.findAll('span', {'class' : 'value'}):
-                        value = item.span.text.strip()
-                        # address information from #0 to #4
-                        if counter == 0:
-                                listing_json['Address1'] =  value
-                        elif counter == 1:
-                                listing_json['Address2'] = value
-                        elif counter == 2:
-                                listing_json['City'] = value
-                        elif counter == 3:
-                                listing_json['Province'] = value
-                        elif counter == 4:
-                                listing_json['PostalCode'] = value
+        
+        # handling the address information
+        listing_json['Address1'] = items[0].span.text.strip()
+        listing_json['Address2'] = items[1].span.text.strip()
+        listing_json['City'] = items[2].span.text.strip()
+        listing_json['Province'] = items[3].span.text.strip()
+        listing_json['PostalCode'] = items[4].span.text.strip()
+ 
+        room_counter = 0; # the line number when handling the room information
+        rooms = [] # the array of rooms
+        room = {} # individual room information
+        is_room = False # if current line is about a room
 
-                if item.label and item.findAll('span', {'class' : 'value'}):
+        # start parsing the MLS line by line
+        for item in items[5:]:
+                
+
+                # handling the (label:value) tuples
+                if item.label and item.findAll('span', {'class' : 'value'}) and (not is_room):
                         label = item.label.text[:-1].strip()
                         value = item.span.text.strip()
                         listing_json[label] = value
-                else:
-                        pass
-                counter = counter + 1
 
+                # handling the room case
+                elif item.text == str(len(rooms)+1):
+                        is_room = True
+
+                elif is_room:
+                        # every room contains 8 lines
+
+                        if room_counter == 0:
+                                room['Number'] = len(rooms)+1
+                        elif room_counter == 1:
+                                room['Name'] = item.span.text.strip()
+                        elif room_counter == 2:
+                                room['Level'] = item.span.text.strip()
+                        elif room_counter == 3:
+                                room['Length'] = item.span.text.strip()
+                        elif room_counter == 4:
+                                room['Width'] = item.span.text.strip()
+                        elif room_counter == 5:
+                                room['Feature1'] = item.span.text.strip()
+                        elif room_counter == 6:
+                                room['Feature2'] = item.span.text.strip()
+                        elif room_counter == 7:
+                                room['Feature3'] = item.span.text.strip()
+
+                        # reset 
+                        if len(room) == 7:
+                                rooms.append(room)
+                                is_room = False
+                                room = {}
+                                room_counter = 0
+                        else:
+                                room_counter = room_counter + 1
+
+        # assign the room information
+        listing_json['Rooms'] = rooms
 
         print listing_json['MLS#']
-        print(json.dumps(listing_json, indent = 4))
+        print(json.dumps(listing_json, indent = 2))
